6397a1b6b965f124b19fcb28f6664d9fe68f88f6
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-12-26 11:07:54 -0500

    V12

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Link, Slot } from "expo-router";
+import { Link, Slot, useLocalSearchParams } from "expo-router";
 import { StyleSheet, View, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
@@ -6,13 +6,16 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function HomeLayout() {
+  const obj = useLocalSearchParams();
+  console.log(obj);
+
   return (
     <View style={styles.container}>
       <View style={styles.navbar}>
         <Link href="/">
           <Feather name="home" size={24} color="black" />
         </Link>
-        <Link href="/">
+        <Link href="/home">
           <Entypo name="news" size={22} color="black" />
         </Link>
         <Link href="/">
