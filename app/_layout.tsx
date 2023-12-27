083757cbe6e433cf21a4af8ec8ce84f8e1faa668
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-12-27 15:16:14 -0500

    Cleaning the _layout.tsx component body

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -51,21 +51,6 @@ export default function HomeLayout() {
       </View>
       <Slot />
     </View>
-    // <Stack
-    //   // https://reactnavigation.org/docs/headers#sharing-common-options-across-screens
-    //   screenOptions={{
-    //     headerStyle: {
-    //       backgroundColor: "#f4511e",
-    //     },
-    //     headerTintColor: "#fff",
-    //     headerTitleStyle: {
-    //       fontWeight: "bold",
-    //     },
-    //   }}
-    // >
-    //   {/* Optionally configure static options outside the route. */}
-    //   <Stack.Screen name="home" options={{}} />
-    // </Stack>
   );
 }
 
