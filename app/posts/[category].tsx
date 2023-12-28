8d6b05f31422a00fda0805527bf6a6ed89b590a9
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-12-28 08:58:16 -0500

    Typing - V24

--- app/posts/[category].tsx
+++ app/posts/[category].tsx
@@ -1,9 +1,50 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { usePathname } from "expo-router";
+
+interface postDetail {
+  // id     String   @id @default(uuid())
+  idUser: String;
+  User: User;
+  idPost: String;
+  Post: Post;
+  iLove: Boolean;
+  iLike: Boolean;
+}
+
+interface User {
+  id: String;
+  name: String;
+  surname: String;
+  email: String;
+  password: String;
+  headquarter: String;
+  cycle: String;
+  carrer: String;
+  Access: any;
+  postDetail: postDetail[];
+}
+
+interface Post {
+  id: String;
+  title: String;
+  description: String;
+  image: any;
+  postDetail: Array<postDetail>;
+  // postDetail: postDetail[];
+  postScope: any;
+}
 
 const Category = () => {
   const pathname = usePathname();
+
+  const [posts, setPosts] = useState<Array<Post>>([]);
+
+  useEffect(() => {
+    fetch("https://utp-app.vercel.app/post/posts/api?idUser=&category=all")
+      .then((r) => r.json())
+      .then((r) => console.log(r));
+  }, []);
 
   return (
     <View style={styles.container}>
